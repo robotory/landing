09adb21dae99e9bb384d3991f49d5d04c6c6945b
Ahmad Vegah <ahmadvegah@gmail.com>
2021-12-08 12:16:22 +0000

    removed unnecessary links and info

--- src/footer/FooterCopyright.tsx
+++ src/footer/FooterCopyright.tsx
@@ -6,12 +6,6 @@ const FooterCopyright = () => (
     <span role="img" aria-label="Love">
       ❤️
     </span>{' '}
-    {/*
-     * PLEASE READ THIS SECTION
-     * We'll really appreciate if you could have a link to our website
-     * The link doesn't need to appear on every pages, one link on one page is enough.
-     * Thank you for your support it'll mean a lot for us.
-     */}
     <style jsx>
       {`
         .footer-copyright :global(a) {
