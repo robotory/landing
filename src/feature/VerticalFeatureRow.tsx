f1b8492b20c8d28732b7ad1a3c245a81ec504ee2
Ahmad Vegah <ahmadvegah@gmail.com>
2021-12-08 03:52:14 +0000

    remove title from features

--- src/feature/VerticalFeatureRow.tsx
+++ src/feature/VerticalFeatureRow.tsx
@@ -2,7 +2,6 @@ import className from 'classnames';
 import { useRouter } from 'next/router';
 
 type IVerticalFeatureRowProps = {
-  title: string;
   description: string;
   image: string;
   imageAlt: string;
@@ -25,7 +24,6 @@ const VerticalFeatureRow = (props: IVerticalFeatureRowProps) => {
   return (
     <div className={verticalFeatureClass}>
       <div className="w-full sm:w-1/2 text-center sm:px-6">
-        <h3 className="text-3xl text-gray-900 font-semibold">{props.title}</h3>
         <div className="mt-6 text-xl leading-9">{props.description}</div>
       </div>
 
